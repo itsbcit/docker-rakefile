1737faf78d1bcbfe892807ce262a6fc50a509816
Jesse Weisner <jesse_weisner@bcit.ca>
2019-07-23 14:01:26 -0700

    DockerImage class

--- lib/DockerImage.rb
+++ lib/DockerImage.rb
@@ -1,15 +1,36 @@
 class DockerImage
-    def initialize()
-        @name = ''
-        @tags = []
-        @variant = ''
-        @version = ''
-        @dir = ''
-        @build_id = ''
-        @registry = ''
-        @maintainer = ''
-        @tini_version = ''
-        @de_version = ''
-        @dockerize_version = ''
+    attr_accessor :name, :tags, :variant, :version, :build_id, :registries,
+                  :org_name, :maintainer, :vars
+    def initialize(
+        name:,
+        org_name:,
+        build_id:   '',
+        tags:       [],
+        version:    '',
+        variant:    '',
+        registries: [],
+        labels:     [],
+        vars:       {}
+    )
+        @name       = name
+        @org_name   = org_name
+        @build_id   = build_id
+        @tags       = tags
+        @version    = version
+        @variant    = variant
+        @registries = registries
+        @labels     = labels
+        @vars       = vars
+    end
+
+    def dir
+        if self.variant.empty? and self.version.empty?
+            dir = './'
+        else
+            # format directory or './' based on version,variant
+            dash = variant.empty? ? '' : '-'
+            dir = "#{version}#{dash}#{variant}"
+        end
+        return dir
     end
 end
