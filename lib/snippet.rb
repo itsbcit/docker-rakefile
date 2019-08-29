597c05a1a9eb96dc39191d8e14eaa97c11fb8f67
Jesse Weisner <jesse_weisner@bcit.ca>
2019-08-29 13:43:18 -0700

    implemented ERB snippets

---
+++ lib/snippet.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# returns output of ERB template in lib/snippets or local/snippets
+def snippet(snippet_name, scope)
+  snippets_file = "snippets/#{snippet_name}.erb"
+  libdir = File.exist?("local/#{snippets_file}") ? 'local' : 'lib'
+  template = "#{libdir}/#{snippets_file}"
+
+  if File.exist?(template)
+    output = ERB.new(File.read(template), 0, '<>-', "_sub").result(scope)
+  else
+    output = ''
+    puts "WARNING: snippet \"#{snippet_name}\" not found.".red
+  end
+
+  output
+end
