90639ca3506f7c2012b6bea52b95d3f93e9a7f5f
Jesse Weisner <jesse@weisner.ca>
2021-10-08 16:08:06 -0700

    fixed missing maintainer merge

--- lib/build_objects_array.rb
+++ lib/build_objects_array.rb
@@ -8,6 +8,7 @@ def build_objects_array(options = {})
   image_name     = metadata['image_name']
   template_files = metadata['template_files'].nil? ? []           : metadata['template_files']
   labels         = metadata['labels'].nil?         ? []           : metadata['labels']
+  maintainer     = metadata['maintainer'].nil?     ? ''           : metadata['maintainer']
   registries     = metadata['registries'].nil?     ? []           : metadata['registries']
   suffixes       = metadata['suffixes'].nil?       ? []           : metadata['suffixes']
   variants       = metadata['variants'].nil?       ? { '' => {} } : metadata['variants']
@@ -18,6 +19,7 @@ def build_objects_array(options = {})
     version_params         = version_params.nil?                   ? {} : version_params
     version_template_files = version_params['template_files'].nil? ? [] : version_params['template_files']
     version_labels         = version_params['labels'].nil?         ? [] : version_params['labels']
+    version_maintainer     = version_params['maintainer'].nil?     ? maintainer : version_params['maintainer']
     version_registries     = version_params['registries'].nil?     ? [] : version_params['registries']
     version_suffixes       = version_params['suffixes'].nil?       ? [] : version_params['suffixes']
     version_variants       = version_params['variants'].nil?       ? [] : version_params['variants']
@@ -29,6 +31,7 @@ def build_objects_array(options = {})
       variant_params         = variant_params.nil?                   ? {} : variant_params
       variant_template_files = variant_params['template_files'].nil? ? [] : variant_params['template_files']
       variant_labels         = variant_params['labels'].nil?         ? {} : variant_params['labels']
+      variant_maintainer     = variant_params['maintainer'].nil?     ? maintainer : variant_params['maintainer']
       variant_registries     = variant_params['registries'].nil?     ? [] : variant_params['registries']
       variant_suffixes       = variant_params['suffixes'].nil?       ? [] : variant_params['suffixes']
       variant_vars           = variant_params['vars'].nil?           ? {} : variant_params['vars']
@@ -42,6 +45,7 @@ def build_objects_array(options = {})
         suffixes:       (suffixes + version_suffixes + variant_suffixes).uniq,
         registries:     merge_registries(registries, version_registries, variant_registries),
         labels:         labels.deep_merge(version_labels).deep_merge(variant_labels),
+        maintainer:     maintainer,
         vars:           vars.deep_merge(version_vars).deep_merge(variant_vars),
       )
     end
